55b811e1b5fb7cc0a4906ee900612124ecd7f97d
gouresan <santhosh.goureshetti@diconium.com>
2025-05-14 12:43:28 +0200

    configure - rate limiter, retries

--- accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -6,6 +6,7 @@ import com.eazybytes.accounts.dto.CustomerDto;
 import com.eazybytes.accounts.dto.ErrorResponseDto;
 import com.eazybytes.accounts.dto.ResponseDto;
 import com.eazybytes.accounts.service.IAccountService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -129,10 +130,17 @@ public class AccountsController {
     }
 
     @GetMapping("/envInfo")
+    @RateLimiter(name="getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     public ResponseEntity<String> getEnvironmentInfo(){
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Java 17");
     }
 
     @GetMapping("/contactInfo")
