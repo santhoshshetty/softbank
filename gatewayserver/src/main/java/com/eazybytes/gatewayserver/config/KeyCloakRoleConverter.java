afe58bfa902c1f47e9e3e93b4b06c7d91da13984
gouresan <santhosh.goureshetti@diconium.com>
2025-06-18 04:04:21 +0200

    Microservices Security

---
+++ gatewayserver/src/main/java/com/eazybytes/gatewayserver/config/KeyCloakRoleConverter.java
@@ -0,0 +1,29 @@
+package com.eazybytes.gatewayserver.config;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+//This class converts the roles we get from KeyCloak to a format that Spring Security understands
+//It needs in the type of GrantedAuthority and internally spring stores the roles with prefix ROLE_, so prefixing it
+public class KeyCloakRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt source) {
+        Map<String, Object> realmAccess = (Map<String, Object>) source.getClaims().get("realm_access");
+        if (realmAccess == null || realmAccess.isEmpty()) {
+            return new ArrayList<>();
+        }
+        return ((List<String>) realmAccess.get("roles"))
+                .stream().map(roleName -> "ROLE_" + roleName)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+}
